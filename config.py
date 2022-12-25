d87600002ceab29eb334529a3b31f43ed4e447c5
Pranav Minasandra <pminasandra@ab.mpg.de>
2022-12-25 18:15:10 +0100

    Finished initialisation

---
+++ config.py
@@ -0,0 +1,27 @@
+# Pranav Minasandra
+# pminasandra.github.io
+# Dec 24, 2022
+
+import os
+import os.path
+
+
+#Directories
+PROJECTROOT = os.path.abspath("..")
+DATA = os.path.join(PROJECTROOT, "Data/")
+FIGURES = os.path.join(PROJECTROOT, "Figures/")
+
+
+# Species
+species = ['hyena', 'meerkat'] #TODO: Eventually, 'coati' also goes here
+for s in species:
+    assert s in os.listdir(DATA)
+    assert os.path.isdir(os.path.join(DATA, s))
+
+
+# Image saving
+formats = ['png', 'svg', 'pdf']
+
+if __name__=="__main__":
+    import utilities
+    utilities.sprint(f"config.py speaking. current projectroot is {PROJECTROOT}")
