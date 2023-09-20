c199822eb03caa1e1b968c64f99144c157bc7c57
Pranav 'Baba' Minasandra <pminasandra@ab.mpg.de>
2023-09-20 17:21:41 +0200

    Generated figure for Supporting Information - SSyn

--- simulate.py
+++ simulate.py
@@ -1,8 +1,14 @@
 
 import matplotlib.pyplot as plt
 
+import config
+import utilities
+
 import simulations
 import simulations.social
+
+if not config.SUPPRESS_INFORMATIVE_PRINT:
+    print = utilities.sprint
 
 #simulations.simulate_with_distribution("Exponential")
 #simulations.simulate_with_distribution("Power_Law")
@@ -14,4 +20,6 @@ for i in range(10):
     print(f"Social simulation, iteration {i}")
     simulations.social.social_sync_simulation(fig, ax)
 
-plt.show()
+ax.set_xlabel("Time since start of bout")
+ax.set_ylabel("Hazard rate")
+utilities.saveimg(fig, "social_reinforcement_simulation")
