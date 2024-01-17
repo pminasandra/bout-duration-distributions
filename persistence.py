1fcf40b3f14ced89a15349295000bcb53243d84f
Pranav 'Baba' Minasandra <pminasandra@ab.mpg.de>
2024-01-17 13:25:35 +0100

    Linting and docs for persistence

--- persistence.py
+++ persistence.py
@@ -3,9 +3,11 @@
 # Sep 27, 2023
 
 """
-Implements Detrended Fluctuation Analysis and Predictive Information for behavioral data.
+Implements Detrended Fluctuation Analysis and Predictive Information for
+behavioural data.
 Available functions:
-    1. generate_time_series: Generates a time-series of an indicator function for the given state.
+    1. generate_time_series: Generates a time-series of an indicator function
+        for the given state.
     2. alpha_DFA: computes DFA alpha exponent
     3. compute_all_alpha_dfa: performs the analysis for all data
     4. save_data
@@ -34,7 +36,8 @@ def generate_time_series(dataframe, species, state):
     """
     Generates a time-series of an indicator function for the given state.
     Args:
-        dataframe (pd.DataFrame): bout information, typically output from boutparsing.bouts_data_generator(...).
+        dataframe (pd.DataFrame): bout information, typically output from
+            boutparsing.bouts_data_generator(...).
         species (str): name of species from which the data come.
         state (str): name of the behavioural state.
     Returns:
@@ -42,9 +45,6 @@ def generate_time_series(dataframe, species, state):
     """
 
     # first generate a 1, -1 time series
-    xmin = config.xmin
-    epoch = classifier_info.classifiers_info[species].epoch
-
     timeseries = np.zeros(shape=len(dataframe["state"]))
     timeseries[dataframe["state"] == state] = 1
     timeseries[dataframe["state"] != state] = -1
@@ -57,7 +57,8 @@ def alpha_dfa(timeseries, integrate=False):
     Generates the value of alpha for the time-series. (see docs)
     Args:
         timeseries (np.array): from generate_time_series
-        integrate (bool): default True, whether to integrate the -1,1 sequence of behaviours. (See the paper)
+        integrate (bool): default False, whether to integrate the -1,1 sequence
+            of behaviours. (See the paper)
     """
     if integrate:
         timeseries = timeseries.cumsum()
@@ -80,9 +81,11 @@ def compute_all_alpha_dfa(integrate=False):
     """
     Performs DFA and stores the exponent for all individuals and states.
     Args:
-        integrate (bool): whether to integrate the time-series prior to performing the analysis (default False).
+        integrate (bool): whether to integrate the time-series prior to
+            performing the analysis (default False).
     Returns:
-        list: elements of list are tuples of the form (species, state, ind_id, alpha_DFA)
+        list: elements of list are tuples of the form
+            (species, state, ind_id, alpha_DFA)
     """
 
     manager = mp.Manager()
@@ -110,7 +113,9 @@ def compute_all_alpha_dfa(integrate=False):
 
 
 def save_dfa_data(results):
-
+    """
+    Saves results from compute_all_alpha_dfa(...)
+    """
     unique = lambda iter_: list(set(iter_))
     unique_species = unique([x[0] for x in results])
 
@@ -127,13 +132,15 @@ def save_dfa_data(results):
             res_sub2 = [res for res in res_sub1 if res[1] == state]
 
             for id_ in unique_ids:
-                singleton_res = [res for res in res_sub2 if res[2] == id_] # should be just one value by now
+                # should be just one value by now
+                singleton_res = [res for res in res_sub2 if res[2] == id_]
                 assert len(singleton_res) == 1
                 dict1[state][id_] = singleton_res[0][-1]
 
         spdata = pd.DataFrame(dict1)
         print("\n", spdata, sep="")
-        spdata.to_csv(os.path.join(config.DATA, f"DFA_{species}.csv"), index_label="id")
+        spdata.to_csv(os.path.join(config.DATA, f"DFA_{species}.csv"),
+                        index_label="id")
 
 
 def _time_slots_for_sampling(Tmin, Tmax, num):
@@ -164,7 +171,7 @@ def _validated_paired_indices(dt_col, tdiff, indices_start, indices_end, epoch):
     all_tdiffs *= 1e-9#because numpy measures time in ns for some reason
     all_tdiffs = all_tdiffs.astype(float)
 # it sucks to work so much against numpy and pandas in a straightforward implementation of something
-    
+ 
 
     mask =  all_tdiffs == tdiff*epoch #mask
     return mask
@@ -272,22 +279,36 @@ def _save_best_dist_params(funcs, params, r2vals):
             best_params = paramset
 
     if best_fit == "exponential_fit":
-        return {"best_fit":best_fit, "coefficient": best_params[0], "power_exponent": np.nan, "exponential_decay": best_params[1]}
-    elif best_fit == "powerlaw_fit":
-        return {"best_fit":best_fit, "coefficient": best_params[0], "power_exponent": best_params[1], "exponential_decay": np.nan}
+        return {"best_fit":best_fit,
+                    "coefficient": best_params[0],
+                    "power_exponent": np.nan,
+                    "exponential_decay": best_params[1]}
+
+    if best_fit == "powerlaw_fit":
+        return {"best_fit":best_fit,
+                    "coefficient": best_params[0],
+                    "power_exponent": best_params[1],
+                    "exponential_decay": np.nan}
+
     if best_fit == "truncated_powerlaw_fit":
-        return {"best_fit":best_fit, "coefficient": best_params[0], "power_exponent": best_params[1], "exponential_decay": best_params[2]}
+        return {"best_fit":best_fit,
+                    "coefficient": best_params[0],
+                    "power_exponent": best_params[1],
+                    "exponential_decay": best_params[2]}
 
 def complete_MI_analysis():
     """
     Runs all analyses for MI decay.
     """
 
+# Load data and inititalise
     print("Mutual Information decay analysis initiated.")
 
     bdg = boutparsing.bouts_data_generator(extract_bouts=False)
     timelags = _time_slots_for_sampling(1, 5000, 50)
-    timelags = np.unique(timelags) #the first value is duplicated b/c log-scaling + rounding
+    timelags = np.unique(timelags)
+    # the first value is duplicated b/c log-scaling + rounding, therefore
+    # unique()
 
     print("complete_MI_analysis: will work on the following lags: ", *timelags)
     plots = {}
@@ -298,6 +319,7 @@ def complete_MI_analysis():
         id_ = databundle["id"]
         data = databundle["data"]
 
+# Make empty plots
         if species_ not in plots:
             plots[species_] = plt.subplots()
         fig, ax = plots[species_]
@@ -305,16 +327,22 @@ def complete_MI_analysis():
 
         print(f"MI decay analysis working on {species_} {id_}.")
 
+# Compute time-lagged MI values
         mi_vals = mutual_information_decay(data, species_, timelags)
+
+# Make plots of actual MI decay
         ax.plot(timelags, mi_vals, color="black", linewidth=0.7)
         ax.set_xscale("log")
         ax.set_yscale("log")
 
+# Fit candidate functions
         (mexp, lambda_), _ = fit_function(timelags, mi_vals, exponential_fit)
         (malpha, alpha), _ = fit_function(timelags, mi_vals, powerlaw_fit)
-        (mtrunc, talpha, tlambda_), _ = fit_function(timelags, mi_vals, truncated_powerlaw_fit)
+        (mtrunc, talpha, tlambda_), _ = fit_function(timelags, mi_vals,
+                                                        truncated_powerlaw_fit)
 
-        r2s = _R2_best_fits((exponential_fit, powerlaw_fit, truncated_powerlaw_fit),
+        r2s = _R2_best_fits((exponential_fit, powerlaw_fit,
+                                truncated_powerlaw_fit),
                             ((mexp, lambda_),
                             (malpha, alpha),
                             (mtrunc, talpha, tlambda_)), timelags, mi_vals)
@@ -323,7 +351,8 @@ def complete_MI_analysis():
         r2s["id"] = id_
         r2_results.append(r2s)
 
-        pars = _save_best_dist_params((exponential_fit, powerlaw_fit, truncated_powerlaw_fit),
+        pars = _save_best_dist_params((exponential_fit, powerlaw_fit,
+                                        truncated_powerlaw_fit),
                                       ((mexp, lambda_),
                             (malpha, alpha),
                             (mtrunc, talpha, tlambda_)), r2s_raw)
@@ -332,12 +361,25 @@ def complete_MI_analysis():
         pars["id"] = id_
         param_results.append(pars)
 
+
+# Make plots
+        # Commenting these out because all fit to trunc pow-law anyway
         #ax.plot(timelags, _exp_func(timelags, mexp, lambda_), color="blue", linestyle="dotted")
         #ax.plot(timelags, _pl_func(timelags, malpha, alpha), color="red", linestyle="dotted")
-        ax.plot(timelags, _tpl_func(timelags, mtrunc, talpha, tlambda_), color="maroon", linestyle="dotted", linewidth=0.5)
+        ax.plot(timelags, _tpl_func(timelags, mtrunc, talpha, tlambda_),
+                    color="maroon",
+                    linestyle="dotted",
+                    linewidth=0.5
+                )
 
-    pd.DataFrame(r2_results).to_csv(os.path.join(config.DATA, "MI_decay_R2s.csv"), index=False)
-    pd.DataFrame(param_results).to_csv(os.path.join(config.DATA, "MI_decay_params.csv"), index=False)
+    pd.DataFrame(r2_results).to_csv(os.path.join(config.DATA,
+                                                    "MI_decay_R2s.csv"
+                                                ),
+                                            index=False)
+    pd.DataFrame(param_results).to_csv(os.path.join(config.DATA,
+                                                    "MI_decay_params.csv"
+                                                    ),
+                                                index=False)
 
     for species_ in plots:
         fig, ax = plots[species_]
@@ -349,8 +391,6 @@ def complete_MI_analysis():
         ax.set_ylabel("Time-lagged mutual information")
         utilities.saveimg(plots[species_][0], f"MI_decay_{species_}")
         
-
-    
 if __name__ == "__main__":
     complete_MI_analysis()
     results = compute_all_alpha_dfa()
