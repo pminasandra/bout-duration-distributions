1200232ba2d7906a69862cadfd5f85821943021c
Pranav Minasandra (Dell) <pminasandra@ab.mpg.de>
2024-01-26 10:27:15 +0100

    Re-removed stretched exponentials
    They weren't adding too much to the interpretability of the results.
    Removing them leads to cleaner analyses.

--- config.py
+++ config.py
@@ -32,8 +32,9 @@ def all_distributions(fit):
     return {'Exponential': fit.exponential,
             'Lognormal': fit.lognormal,
             'Power_Law': fit.power_law,
-            'Truncated_Power_Law': fit.truncated_power_law,
-            'Stretched_Exponential': fit.stretched_exponential}
+            'Truncated_Power_Law': fit.truncated_power_law#,
+#            'Stretched_Exponential': fit.stretched_exponential
+        }
 
 discrete = True
 xmin = 2.0
@@ -42,8 +43,8 @@ distributions_to_numbers = {
     'Exponential': 0,
     'Lognormal': 1,
     'Power_Law': 2,
-    'Truncated_Power_Law': 3,
-    'Stretched_Exponential': 4
+    'Truncated_Power_Law': 3#,
+#    'Stretched_Exponential': 4
 }
 
 
@@ -52,8 +53,8 @@ colors = {
     'Exponential': 'cyan',
     'Lognormal': 'blue',
     'Power_Law': 'red',
-    'Truncated_Power_Law': 'maroon',
-    'Stretched_Exponential': 'pink'
+    'Truncated_Power_Law': 'maroon'#,
+#    'Stretched_Exponential': 'pink'
 }
 fit_line_style = 'dotted'
 
