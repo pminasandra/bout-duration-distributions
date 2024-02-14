e6cbd05563ddfb85612b610d8b2995ebb67c9fdb
Pranav 'Baba' Minasandra <pminasandra@ab.mpg.de>
2024-02-14 16:34:39 +0100

    Improved setup.sh

--- setup.sh
+++ setup.sh
@@ -7,12 +7,20 @@
 # Usage:
 # curl -sSf https://raw.githubusercontent.com/pminasandra/bout-duration-distributions/master/setup.sh | bash
 
+TOPDIR="$PWD"
+
 mkdir bout-duration-distributions
 cd bout-duration-distributions
 
 git clone https://github.com/pminasandra/bout-duration-distributions code
 
-mkdir Data
+mkdir Data/{,meerkat,hyena,coati}
 mkdir Figures
 
+cd code
 echo $PWD > cwd.txt
+python3 -m pip install setuptools wheel
+python3 -m pip install -e .
+python3 -m pip install -r requirements.txt
+
+cd "$TOPDIR"
