fcddaf7a6caaa33c75e5e0b345dc14bba312ab01
Pranav Minasandra (Dell) <pminasandra@ab.mpg.de>
2025-02-18 10:24:11 +0100

    fixed minor bug

--- pkgnametbd/fitting.py
+++ pkgnametbd/fitting.py
@@ -531,6 +531,5 @@ if __name__ == "__main__":
     if config.COLLAGE_IMAGES:
         plt.rcParams.update({'font.size': 22})
 
-    test_for_powerlaws(add_markov=True,
-        add_markov=config.ADD_MARKOV,
+    test_for_powerlaws(add_markov=config.ADD_MARKOV,
         add_bootstrapping=config.ADD_BOOTSTRAPPING)
